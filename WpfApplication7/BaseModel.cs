2ce303b5df420edb1efdbf15542f27605f1cd6f1
fetyka <tanulo18@terem307pc18.athenepraxis.local>
2017-09-11 19:49:33 +0200

    commitka

---
+++ WpfApplication7/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApplication7
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChange([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfApplication7/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication7
+{
+    public class Person : BaseModel
+    {
+        string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChange();
+            }
+        }
+
+        int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                OnPropertyChange();
+            }
+        }
+    }
+}
